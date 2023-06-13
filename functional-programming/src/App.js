0dc004b0b79c8aaaf56366b137e6647df2d6f438
Bahar Keshavarz <bahar.keshavarzc@gmail.com>
2023-06-13 08:39:58 +0300

    added a recursive component

--- functional-programming/src/App.js
+++ functional-programming/src/App.js
@@ -1,7 +1,32 @@
+import RecursiveComponent from "./RecursiveComponent";
+
+const nestedData  = {
+  a: 1,
+  b: {
+    b1: 4,
+    b2: {
+        b23: 'Hello',
+    },
+    b3: {
+        b31: {
+           message: 'Hi',
+        },
+        b32: {
+           message: 'Hi',
+        }
+    },
+    c: {
+       c1: 10,
+       c2: 20,
+    }
+  }
+}
+
 function App() {
   return (
     <div className="App">
-      
+      { nestedData && <ul><RecursiveComponent data={nestedData}/></ul>
+      }
     </div>
   );
 }
